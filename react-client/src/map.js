b20ca8c5e9030f9e9b28922e6080e76418b58a3d
Trevor Sasaki <Trevor_sasaki@hotmail.comgit config --global user.email Trevor_sasaki@hotmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf true>
2018-06-07 19:42:27 -0500

    added carousel and other stuff in details page

--- react-client/src/map.js
+++ react-client/src/map.js
@@ -4,8 +4,8 @@ import MAPS_API_KEY from './config';
 const EmbeddedMap = props => (
     <div>
       <iframe
-        width="600"
-        height="450"
+        width="100%"
+        height="675"
         frameBorder="0" style={{ border: '0' }}
         allowFullScreen
       src={`https://www.google.com/maps/embed/v1/directions?key=${MAPS_API_KEY}
